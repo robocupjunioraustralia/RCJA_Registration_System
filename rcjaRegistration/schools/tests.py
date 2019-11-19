5bcabb385e523ee7b91798985fdd127e6329dab3
Dalordish <dalordish@gmail.com>
2019-11-19 17:51:57 +1100

    add some dummy tests

--- rcjaRegistration/schools/tests.py
+++ rcjaRegistration/schools/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+class TestNonTest(TestCase):
+    def testEquality(self):
+        self.assertEqual('foo','foo')
+    def test_truth(self):
+        self.assertTrue(True,True)
+    def test_failure(self):
+        self.assertTrue(False)
+        self.assertFalse(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
 # Create your tests here.
