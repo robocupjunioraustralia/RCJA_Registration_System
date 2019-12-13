66d3ace117c77804813721cf4fc2965009c65f21
Peter Drew <peter@pdrew.com>
2019-12-13 14:59:27 +1100

    add login required decorator

--- rcjaRegistration/competitions/views.py
+++ rcjaRegistration/competitions/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
 
 import datetime
 
@@ -10,6 +11,7 @@ from .models import Competition
 
 # Need to check if mentor is None
 
+@login_required
 def index(request):
     openForRegistrationCompetitions = Competition.objects.filter(registrationsOpenDate__lte=datetime.datetime.today(), registrationsCloseDate__gte=datetime.datetime.today()).exclude(team__school__mentor__user=request.user).order_by('startDate')
     currentCompetitions = Competition.objects.filter(endDate__gte=datetime.datetime.today(), team__school__mentor__user=request.user).distinct().order_by('startDate')
@@ -21,6 +23,7 @@ def index(request):
     }
     return render(request, 'competitions/viewcomp.html', context)
 
+@login_required
 def detail(request, competitionID):
     competition = get_object_or_404(Competition, pk=competitionID)
     from teams.models import Team
