af8549b7fdf55aac3aa824d2888c8692d8dd28ca
Angus Trau <me@angus.ws>
2019-12-21 14:52:39 +1100

    Switch to postgres

--- rcjaRegistration/rcjaRegistration/settings.py
+++ rcjaRegistration/rcjaRegistration/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,10 +79,7 @@ WSGI_APPLICATION = 'rcjaRegistration.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(),
 }
 
 
