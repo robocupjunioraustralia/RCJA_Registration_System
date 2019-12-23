5f6f5fa13fcfe56ff96ec2bc4026dc312f5b9342
Dalordish <dalordish@gmail.com>
2019-12-23 15:38:23 +1100

    add widget tweaks to installed apps

--- rcjaRegistration/rcjaRegistration/settings.py
+++ rcjaRegistration/rcjaRegistration/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'widget_tweaks',
     'regions.apps.RegionsConfig',
     'teams.apps.TeamsConfig',
     'events.apps.EventsConfig',
