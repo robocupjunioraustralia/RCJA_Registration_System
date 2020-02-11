b7b3bf3abf4936312a5ce4d6c69ec3d9d4e9b195
Peter Drew <peter@pdrew.com>
2020-02-11 12:51:39 +1100

    add MentorQuestionResponse

--- rcjaRegistration/mentorquestions/models.py
+++ rcjaRegistration/mentorquestions/models.py
@@ -29,3 +29,32 @@ class MentorQuestion(models.Model):
     # *****CSV export methods*****
 
     # *****Email methods*****
+
+class MentorQuestionResponse(models.Model):
+    # Fields and primary key
+    mentorQuestion = models.ForeignKey(MentorQuestion, verbose_name='Mentor question', on_delete=models.CASCADE)
+    mentor = models.ForeignKey('schools.Mentor', verbose_name='Mentor', on_delete=models.CASCADE)
+    # Creation and update time
+    creationDateTime = models.DateTimeField('Creation date',auto_now_add=True)
+    updatedDateTime = models.DateTimeField('Last modified date',auto_now=True)
+    # Fields
+    response = models.BooleanField('Response')
+
+    # *****Meta and clean*****
+    class Meta:
+        verbose_name = "Mentor Question Response"
+        ordering = ['mentorQuestion']
+        unique_together = ('mentorQuestion', 'mentor')
+
+    # *****Save & Delete Methods*****
+
+    # *****Methods*****
+
+    # *****Get Methods*****
+
+    def __str__(self):
+        return str(self.mentorQuestion)
+
+    # *****CSV export methods*****
+
+    # *****Email methods*****
