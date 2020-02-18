7bbdd5b4305c8af3494c0f957356e5cffbea9e2f
Angus Trau <me@angus.ws>
2020-02-18 12:22:22 +1100

    Hello world test

--- rcjaRegistration/heroku-setup.sh
+++ rcjaRegistration/heroku-setup.sh
@@ -1,2 +1,3 @@
-/app/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+# /app/manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email $DJANGO_SUPERUSER_EMAIL
+echo "hello world"
 exit 0
