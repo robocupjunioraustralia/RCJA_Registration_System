19dbb10522733c3488f27ef26cc2c7b40fca073b
Peter Drew <peter@pdrew.com>
2020-02-23 00:43:12 +1100

    add InvoiceGlobalSettings tests

--- rcjaRegistration/invoices/tests.py
+++ rcjaRegistration/invoices/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import InvoiceGlobalSettings, Invoice, InvoicePayment
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+class TestInvoiceGlobalSettings(TestCase):
+    def setUp(self):
+        pass
+
+    def testStr(self):
+        settingsObj = InvoiceGlobalSettings.objects.create(invoiceFromName='1', invoiceFromDetails='1')
+        self.assertEqual(str(settingsObj), 'Invoice settings')
+
+    def testObject1Limit(self):
+        ob1 = InvoiceGlobalSettings.objects.create(invoiceFromName='1', invoiceFromDetails='1')
+        self.assertEqual(str(ob1), 'Invoice settings')
+
+        obj2 = InvoiceGlobalSettings(invoiceFromName='2', invoiceFromDetails='2')
+        self.assertRaises(ValidationError, obj2.clean)
