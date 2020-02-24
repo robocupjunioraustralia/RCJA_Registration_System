f654e19da6c23f6a16e7cef3953eb5471583d09f
Peter Drew <peter@pdrew.com>
2020-02-24 19:46:51 +1100

    invoice improvements

--- rcjaRegistration/teams/views.py
+++ rcjaRegistration/teams/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404,redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Student,Team
 from django.core.exceptions import ValidationError, PermissionDenied
 from .forms import TeamForm,StudentForm
