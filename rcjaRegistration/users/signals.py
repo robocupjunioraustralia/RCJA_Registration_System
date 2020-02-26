48e27e5f079a2a67e31566b2022813fdb779ce45
Peter Drew <peter@pdrew.com>
2020-02-27 01:55:24 +1100

    user save set school region signal

--- rcjaRegistration/users/signals.py
+++ rcjaRegistration/users/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, pre_save, pre_delete, post_delet
 from django.dispatch import receiver
 
 from schools.models import SchoolAdministrator
+from .models import User
 
 # School administrator post-delete
 # This signal is important for security because many front end checks use the currentlySelectedSchool field to check school permissions.
@@ -16,3 +17,18 @@ def SchoolAdministrator_post_delete(sender, instance, **kwargs):
         else:
             instance.user.currentlySelectedSchool = None
         instance.user.save(update_fields=['currentlySelectedSchool'])
+
+# User post save
+# Used to set state and region on connected school if null
+@receiver(post_save, sender=User)
+def User_post_save(sender, instance, **kwargs):
+    if instance.currentlySelectedSchool:
+        # Set state
+        if instance.currentlySelectedSchool.state is None:
+            instance.currentlySelectedSchool.state = instance.homeState
+            instance.currentlySelectedSchool.save(update_fields=['state'])
+
+        # Set region
+        if instance.currentlySelectedSchool.region is None:
+            instance.currentlySelectedSchool.region = instance.homeRegion
+            instance.currentlySelectedSchool.save(update_fields=['region'])
