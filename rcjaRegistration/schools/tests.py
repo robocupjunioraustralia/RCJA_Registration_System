af7d9d2510050d7170dae2de9ad2d804331951f4
Peter Drew <peter@pdrew.com>
2020-03-03 14:48:15 +1100

    add school detail post tests

--- rcjaRegistration/schools/tests.py
+++ rcjaRegistration/schools/tests.py
@@ -8,6 +8,10 @@ from django.core.exceptions import ValidationError
 from users.models import User
 from .models import School, SchoolAdministrator, Campus
 from regions.models import State, Region
+from events.models import Year, Division, Event
+from invoices.models import Invoice
+
+import datetime
 
 # View Tests
 class TestSchoolCreate(TestCase): #TODO update to use new auth model
@@ -164,10 +168,12 @@ class TestSetCurrentSchool(TestCase):
 
 class TestEditSchoolDetails(TestCase):
     email = 'user@user.com'
+    email2 = 'user2@user.com'
     password = 'chdj48958DJFHJGKDFNM'
 
     def setUp(self):
         self.user = User.objects.create_user(email=self.email, password=self.password)
+        self.user2 = User.objects.create_user(email=self.email2, password=self.password)
 
         self.state1 = State.objects.create(treasurer=self.user, name='Victoria', abbreviation='VIC')
         self.region1 = Region.objects.create(name='Test Region', description='test desc')
@@ -175,6 +181,22 @@ class TestEditSchoolDetails(TestCase):
         self.school1 = School.objects.create(name='School 1', abbreviation='sch1', state=self.state1, region=self.region1)
         self.school2 = School.objects.create(name='School 2', abbreviation='sch2', state=self.state1, region=self.region1)
         self.school3 = School.objects.create(name='School 3', abbreviation='sch3', state=self.state1, region=self.region1)
+
+        self.year = Year.objects.create(year=2020)
+        self.event = Event.objects.create(
+            year=self.year,
+            state=self.state1,
+            name='Test event 1',
+            maxMembersPerTeam=5,
+            entryFeeIncludesGST=True,
+            event_billingType='team',
+            event_defaultEntryFee = 50,
+            startDate=(datetime.datetime.now() + datetime.timedelta(days=5)).date(),
+            endDate = (datetime.datetime.now() + datetime.timedelta(days=5)).date(),
+            registrationsOpenDate = (datetime.datetime.now() + datetime.timedelta(days=-10)).date(),
+            registrationsCloseDate = (datetime.datetime.now() + datetime.timedelta(days=1)).date(),
+            directEnquiriesTo = self.user,
+        )
 
     def testLoginRequired(self):
         url = reverse('schools:details')
@@ -199,6 +221,301 @@ class TestEditSchoolDetails(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 403)
         self.assertContains(response, "You do not have permission to view this school", status_code=403)
+
+    def testChangeName_success(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":0,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':1,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"New name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 302)
+        School.objects.get(name='New name')
+
+    def testChangeName_failure(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":0,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':1,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"SchoOl 2",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'School with this name exists. Please ask your school administrator to add you.')
+
+    def testMissingState_failure(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":0,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':1,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'region': self.region1.id,
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 200)
+
+    def testAddCampus_success(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":0,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':1,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+            'campus_set-0-name': 'First campus',
+            'campus_set-1-name': 'Second campus',
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 302)
+        Campus.objects.get(name='First campus')
+        Campus.objects.get(name='Second campus')
+
+    def testDeleteCampus_success(self):
+        self.campus1 = Campus.objects.create(school=self.school1, name='test 1')
+        Campus.objects.get(name='test 1')
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+
+        payload = {
+            'campus_set-TOTAL_FORMS':3,
+            "campus_set-INITIAL_FORMS":1,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':1,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+            'campus_set-0-id': self.campus1.id,
+            'campus_set-0-name': 'test 1',
+            'campus_set-0-DELETE': 'on',
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 302)
+        self.assertRaises(Campus.DoesNotExist, lambda: Campus.objects.get(name='test 1'))
+
+    def testDeleteCampus_protected(self):
+        self.campus1 = Campus.objects.create(school=self.school1, name='test 1')
+        Invoice.objects.create(event=self.event, invoiceToUser=self.user, school=self.school1, campus=self.campus1)
+        Campus.objects.get(name='test 1')
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+
+        payload = {
+            'campus_set-TOTAL_FORMS':3,
+            "campus_set-INITIAL_FORMS":1,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':1,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+            'campus_set-0-id': self.campus1.id,
+            'campus_set-0-name': 'test 1',
+            'campus_set-0-DELETE': 'on',
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 302)
+        Campus.objects.get(name='test 1')
+
+    def testAdministratorList(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.admin2 = SchoolAdministrator.objects.create(school=self.school1, user=self.user2)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":2,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':2,
+            "schooladministrator_set-INITIAL_FORMS":2,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+        }
+
+        response = self.client.get(url, data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.email2)
+
+    def testAdministratorDelete_success(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.admin2 = SchoolAdministrator.objects.create(school=self.school1, user=self.user2)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":0,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':1,
+            "schooladministrator_set-INITIAL_FORMS":1,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+            'schooladministrator_set-0-id': self.admin2.id,
+            'schooladministrator_set-0-id': self.admin2.user.id,
+            'schooladministrator_set-0-DELETE': 'on',
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 302)
+        self.assertRaises(SchoolAdministrator.DoesNotExist, lambda: SchoolAdministrator.objects.get(pk=self.admin2.pk))
+
+    def testAdministratorAdd_existing(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+        numberExistingAdmins = SchoolAdministrator.objects.count()
+        numberExistingUsers = User.objects.count()
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":0,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':0,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+            'addAdministratorEmail': self.email2.upper(),
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(SchoolAdministrator.objects.count(), numberExistingAdmins+1)
+        self.assertEqual(User.objects.count(), numberExistingUsers)
+        SchoolAdministrator.objects.get(school=self.school1, user=self.user2)
+
+    def testAdministratorAdd_new(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+        numberExistingAdmins = SchoolAdministrator.objects.count()
+        numberExistingUsers = User.objects.count()
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":0,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':0,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+            'addAdministratorEmail': 'new@new.com',
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(SchoolAdministrator.objects.count(), numberExistingAdmins+1)
+        self.assertEqual(User.objects.count(), numberExistingUsers+1)
+        SchoolAdministrator.objects.get(school=self.school1, user__email='new@new.com')
+
+    def testAdministratorAdd_invalidEMail(self):
+        self.admin1 = SchoolAdministrator.objects.create(school=self.school1, user=self.user)
+        self.client.login(request=HttpRequest(), username=self.email, password=self.password)
+        url = reverse('schools:details')
+        numberExistingAdmins = SchoolAdministrator.objects.count()
+        numberExistingUsers = User.objects.count()
+
+        payload = {
+            'campus_set-TOTAL_FORMS':2,
+            "campus_set-INITIAL_FORMS":0,
+            "campus_set-MIN_NUM_FORMS":0,
+            "campus_set-MAX_NUM_FORMS":1000,
+            'schooladministrator_set-TOTAL_FORMS':0,
+            "schooladministrator_set-INITIAL_FORMS":0,
+            "schooladministrator_set-MIN_NUM_FORMS":0,
+            "schooladministrator_set-MAX_NUM_FORMS":1000,
+            "name":"other name",
+            "abbreviation": 'sch1',
+            'state': self.state1.id,
+            'region': self.region1.id,
+            'addAdministratorEmail': 'new',
+        }
+
+        response = self.client.post(url, data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(SchoolAdministrator.objects.count(), numberExistingAdmins)
+        self.assertEqual(User.objects.count(), numberExistingUsers)
 
 def schoolSetUp(self):
     self.user1 = User.objects.create_user(email=self.email1, password=self.password)
