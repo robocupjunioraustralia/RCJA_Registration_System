624143733f2df3606b52a12cf2e1cbc0a423fef9
Peter Drew <peter@pdrew.com>
2020-03-08 12:46:05 +1100

    Add user and user questions state based permissions (Closes #223)

--- rcjaRegistration/users/admin.py
+++ rcjaRegistration/users/admin.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import Group
-from .models import User
-from coordination.adminPermissions import AdminPermissions
+
 from common.admin import ExportCSVMixin
+from coordination.adminPermissions import AdminPermissions, FilteredFKForm
+
+from .models import User
+
+from userquestions.admin import QuestionResponseInline
 
 # Unregister group
-
 admin.site.unregister(Group)
 
 # User admin
@@ -48,6 +51,14 @@ class User_QuestionResponse_Filter(admin.SimpleListFilter):
         except (ValueError,TypeError):
             return queryset
 
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+
+class Filtered_UserChangeForm(FilteredFKForm, UserChangeForm):
+    pass
+
+class Filtered_UserCreationForm(FilteredFKForm, UserCreationForm):
+    pass
+
 @admin.register(User)
 class UserAdmin(AdminPermissions, DjangoUserAdmin, ExportCSVMixin):
     """Define admin model for custom User model with no email field."""
@@ -67,7 +78,7 @@ class UserAdmin(AdminPermissions, DjangoUserAdmin, ExportCSVMixin):
             'is_active',
             'is_staff',
             'is_superuser',
-            'groups',
+            # 'groups',
             'user_permissions'
         )}),
         (_('Flags'), {'fields': (
@@ -86,6 +97,16 @@ class UserAdmin(AdminPermissions, DjangoUserAdmin, ExportCSVMixin):
                 'email',
                 'password1',
                 'password2'
+            ),
+        }),
+        (_('Personal info'), {
+            'classes': ('wide',),
+            'fields': (
+                'first_name',
+                'last_name',
+                'mobileNumber',
+                'homeState',
+                'homeRegion',
             ),
         }),
     )
@@ -122,12 +143,6 @@ class UserAdmin(AdminPermissions, DjangoUserAdmin, ExportCSVMixin):
         'homeRegion',
         User_QuestionResponse_Filter,
     )
-    from userquestions.admin import QuestionResponseInline
-    inlines = [
-        SchoolAdministratorInline,
-        CoordinatorInline,
-        QuestionResponseInline,
-    ]
     autocomplete_fields = [
         'homeState',
     ]
@@ -159,16 +174,42 @@ class UserAdmin(AdminPermissions, DjangoUserAdmin, ExportCSVMixin):
                 'field': 'homeState',
                 'queryset': State.objects.filter(
                     coordinator__user=request.user,
-                    coordinator__permissions__in=reversePermisisons(User, ['add', 'change'])
+                    coordinator__permissions='full',
                 )
             }
         ]
+
+    form = Filtered_UserChangeForm
+    add_form = Filtered_UserCreationForm
 
     def stateFilteringAttributes(self, request):
         from coordination.models import Coordinator
         return {
             'homeState__coordinator__in': Coordinator.objects.filter(user=request.user)
         }
+
+    # Only superuser can change permissions on users
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.is_superuser:
+            return self.readonly_fields
+        return self.readonly_fields + ('is_staff','is_superuser', 'is_active')
+
+    def get_inlines(self, request, obj):
+        # Don't want to show inline on create user page
+        if obj is None:
+            return []
+
+        # Only superuser can edit inlines on admin
+        if request.user.is_superuser:
+            return [
+            SchoolAdministratorInline,
+            CoordinatorInline,
+            QuestionResponseInline,
+        ]
+        return [
+            QuestionResponseInline,
+        ]
+
 
     # Actions
 
