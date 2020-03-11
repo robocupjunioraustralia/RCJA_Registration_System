eb1c5bedc26ff34d843e0e28d5ae692b3cfe1f81
Peter Drew <peter@pdrew.com>
2020-03-11 11:56:17 +1100

    fix invoice tests

--- rcjaRegistration/invoices/tests.py
+++ rcjaRegistration/invoices/tests.py
@@ -980,7 +980,7 @@ class TestInvoiceCalculations_NoCampuses(TestCase):
         self.event.save()
 
         self.assertEqual(self.invoice.invoiceAmountExclGST(), round(12 * 50 - 80, 2))
-        self.assertEqual(self.invoice.invoiceGST(), round((12 * 50 - 80)*0.1, 2))
+        self.assertEqual(self.invoice.amountGST(), round((12 * 50 - 80)*0.1, 2))
         self.assertEqual(self.invoice.invoiceInclGST(), round((12 * 50 - 80)*1.1, 2))
 
     def testAvailableDivisionRateTeam(self):
