2ff1300b5b52661d8388d3b0c730c9014403cac4
Peter Drew <peter@pdrew.com>
2020-03-15 17:10:40 +1100

    refactor team edit and create views

--- rcjaRegistration/teams/urls.py
+++ rcjaRegistration/teams/urls.py
@@ -5,5 +5,5 @@ app_name = 'teams'
 urlpatterns = [
     path('events/<int:eventID>/createTeam', views.CreateTeam.as_view(), name='create'),
     path('teams/<int:teamID>/delete', views.deleteTeam, name='delete'),
-    path('teams/<int:teamID>/edit', views.editTeam, name='edit')
+    path('teams/<int:teamID>/edit', views.EditTeam.as_view(), name='edit')
 ]
--- rcjaRegistration/teams/views.py
+++ rcjaRegistration/teams/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.core.exceptions import ValidationError, PermissionDenied
 from django.contrib.auth.decorators import login_required
 from django.forms import modelformset_factory, inlineformset_factory
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseForbidden
+from django.http import HttpResponse, HttpResponseForbidden
 from django.urls import reverse
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -15,61 +15,6 @@ from events.models import Event
 import datetime
 
 # Create your views here.
-
-class CreateTeam(LoginRequiredMixin, View):
-    def common(self, request, eventID):
-        event = get_object_or_404(Event, pk=eventID)
-
-        if not event.eventType == 'competition':
-            return HttpResponseNotFound('Teams cannot be created for this event type')
-
-        self.StudentInLineFormSet = inlineformset_factory(Team, Student, form=StudentForm, extra=event.maxMembersPerTeam, max_num=event.maxMembersPerTeam, can_delete=False)
-
-        # Check registrations open
-        if event.registrationsCloseDate < datetime.datetime.now().date():
-            raise PermissionDenied("Registrtaion has closed for this event")
-        
-        return event
-
-    def get(self, request, eventID):
-        event = self.common(request, eventID)
-
-        # Get default campus if only one campus for school
-        if request.user.currentlySelectedSchool:
-            campuses = request.user.currentlySelectedSchool.campus_set.all()
-            defaultCampusID = campuses.first().id if campuses.count() == 1 else None
-        else:
-            defaultCampusID = None
-
-        # Create form
-        form = TeamForm(user=request.user, event=event, initial={'campus':defaultCampusID})
-        formset = self.StudentInLineFormSet()
-
-        return render(request, 'teams/addEditTeam.html', {'form': form, 'formset':formset, 'event':event})
-
-    def post(self, request, eventID):
-        event = self.common(request, eventID)
-
-        formset = self.StudentInLineFormSet(request.POST)
-        form = TeamForm(request.POST, user=request.user, event=event)
-        form.mentorUser = request.user # Needed in form validation to check number of teams for independents not exceeded
-
-        if form.is_valid() and formset.is_valid():
-            # Create team object but don't save so can set foreign keys
-            team = form.save(commit=False)
-            team.mentorUser = request.user
-
-            # Save team
-            team.save()
-
-            # Save student formset
-            formset.instance = team
-            formset.save() 
-            if 'add_text' in request.POST:
-                return redirect(reverse('teams:create', kwargs = {"eventID":event.id}))
-            return redirect(reverse('events:details', kwargs = {'eventID':event.id}))
-
-        return render(request, 'teams/addEditTeam.html', {'form': form, 'formset':formset, 'event':event})
 
 def teamPermissions(request, team):
     if request.user.currentlySelectedSchool:
@@ -84,24 +29,44 @@ def teamPermissions(request, team):
     
     return True
 
-@login_required
-def editTeam(request, teamID):
-    team = get_object_or_404(Team, pk=teamID)
-    event = team.event
+class CreateEditTeam(LoginRequiredMixin, View):
+    def common(self, request, event, team=None):
+        # Check is competition for team
+        if not event.eventType == 'competition':
+            raise PermissionDenied('Teams cannot be created for this event type')
 
-    # Check registrations open
-    # TODO: Add a view page or move this to the post
-    if event.registrationsCloseDate < datetime.datetime.now().date():
-        raise PermissionDenied("Registrtaion has closed for this event!")
+        # Check registrations open
+        if event.registrationsCloseDate < datetime.datetime.now().date():
+            raise PermissionDenied("Registrtaion has closed for this event!")
 
-    # Check administrator of this team
-    if not teamPermissions(request, team):
-        raise PermissionDenied("You are not an administrator of this team")
+        # Check administrator of this team
+        if team and not teamPermissions(request, team):
+            raise PermissionDenied("You are not an administrator of this team")
 
-    StudentInLineFormSet = inlineformset_factory(Team,Student,form=StudentForm,extra=event.maxMembersPerTeam,max_num=event.maxMembersPerTeam,can_delete=True)
+        self.StudentInLineFormSet = inlineformset_factory(
+            Team,
+            Student,
+            form = StudentForm,
+            extra = event.maxMembersPerTeam,
+            max_num = event.maxMembersPerTeam,
+            can_delete = team is not None,
+        )
 
-    if request.method == 'POST':
-        formset = StudentInLineFormSet(request.POST, instance=team)
+    def get(self, request, event, team=None):
+        self.common(request, event, team)
+
+        # Get form
+        form = TeamForm(instance=team, user=request.user, event=event)
+        formset = self.StudentInLineFormSet(instance=team)
+
+        return render(request, 'teams/addEditTeam.html', {'form': form, 'formset':formset, 'event':event, 'team':team})
+
+    def post(self, request, event, team=None):
+        self.common(request, event, team)
+
+        newTeam = team is None
+
+        formset = self.StudentInLineFormSet(request.POST, instance=team)
         form = TeamForm(request.POST, instance=team, user=request.user, event=event)
         form.mentorUser = request.user # Needed in form validation to check number of teams for independents not exceeded
 
@@ -114,13 +79,38 @@ def editTeam(request, teamID):
             team.save()
 
             # Save student formset
-            formset.save() 
+            if newTeam:
+                formset.instance = team
+            formset.save()
 
-            return redirect(reverse('events:details', kwargs = {"eventID":event.id}))
-    else:
-        form = TeamForm(instance=team, user=request.user, event=event)
-        formset = StudentInLineFormSet(instance=team)
-    return render(request, 'teams/addEditTeam.html', {'form': form, 'formset':formset, 'event':event, 'team':team})
+            # Redirect if add another in response
+            if 'add_text' in request.POST and newTeam:
+                return redirect(reverse('teams:create', kwargs = {"eventID":event.id}))
+
+            return redirect(reverse('events:details', kwargs = {'eventID':event.id}))
+
+        # Default to displaying the form again if form not valid
+        return render(request, 'teams/addEditTeam.html', {'form': form, 'formset':formset, 'event':event})
+
+class CreateTeam(CreateEditTeam):
+    def get(self, request, eventID):
+        event = get_object_or_404(Event, pk=eventID)
+        return super().get(request, event)
+
+    def post(self, request, eventID):
+        event = get_object_or_404(Event, pk=eventID)
+        return super().post(request, event)
+
+class EditTeam(CreateEditTeam):
+    def get(self, request, teamID):
+        team = get_object_or_404(Team, pk=teamID)
+        event = team.event
+        return super().get(request, event, team)
+
+    def post(self, request, teamID):
+        team = get_object_or_404(Team, pk=teamID)
+        event = team.event
+        return super().post(request, event, team)
 
 @login_required
 def deleteTeam(request, teamID): 
