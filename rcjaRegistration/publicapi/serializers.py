ec8be2cf4ba5ddd2218490ee783e292d85e9b306
Peter Drew <peter@pdrew.com>
2020-05-02 12:32:18 +1000

    add venue and division serializers

--- rcjaRegistration/publicapi/serializers.py
+++ rcjaRegistration/publicapi/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from events.models import Event
+from events.models import Event, AvailableDivision, Venue
 from regions.models import State, Region
 
 # *****Regions*****
@@ -16,8 +16,38 @@ class StateSerializer(serializers.ModelSerializer):
 
 # *****Events*****
 
+class AvailableDivisionSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(label='Name', source='division.name')
+    description = serializers.CharField(label='Description', source='division.description')
+
+    class Meta:
+        model = AvailableDivision
+        fields = [
+            'id',
+            'name',
+            'description',
+            # Team details
+            'division_maxTeamsPerSchool',
+            'division_maxTeamsForDivision',
+            # Billing details
+            'division_billingType',
+            'division_entryFee',
+        ]
+
+class VenueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venue
+        fields = [
+            'id',
+            'name',
+            'address',
+        ]
+
 class EventSerializer(serializers.ModelSerializer):
     state = StateSerializer(read_only=True)
+    availabledivision_set = AvailableDivisionSerializer(read_only=True, many=True)
+    venue = VenueSerializer(read_only=True)
+
     class Meta:
         model = Event
         fields = [
@@ -53,5 +83,5 @@ class EventSerializer(serializers.ModelSerializer):
             'eventDetails',
             'additionalInvoiceMessage',
             # Divisions
-            'divisions',
+            'availabledivision_set',
         ]
