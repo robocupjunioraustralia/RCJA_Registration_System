3183ac5ed3ba17af891a4b26094379805b7efe4e
Peter Drew <peter@pdrew.com>
2020-10-09 21:00:23 +1100

    add MentorEventAttendanceFile

--- rcjaRegistration/eventfiles/models.py
+++ rcjaRegistration/eventfiles/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from common.models import *
+from django.conf import settings
 
+import uuid
+from django.utils.html import format_html
+from common.utils import formatFilesize
+
+from rcjaRegistration.storageBackends import PrivateMediaStorage
+
+# **********MODELS**********
+
+class MentorEventAttendanceFile(models.Model):
+    # Foreign keys
+    eventAttendance = models.ForeignKey('events.BaseEventAttendance', verbose_name='Event Attendance', on_delete=models.PROTECT) # Not sure how to handle deletions. Ideally need to delete the file in S3.
+    uploadedBy = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Uploaded by', on_delete=models.PROTECT, editable=False)
+
+    # Creation and update time
+    creationDateTime = models.DateTimeField('Creation date',auto_now_add=True)
+    updatedDateTime = models.DateTimeField('Last modified date',auto_now=True)
+
+    # Fields
+
+    # Banner image
+    def generateUUIDFilename(self, filename):
+        self.originalFileName = filename
+        extension = filename.rsplit('.', 1)[1]
+        newFilename = f'MentorFile_{str(uuid.uuid4())}.{extension}'
+        return newFilename
+    fileUpload = models.ImageField('File', storage=PrivateMediaStorage(), upload_to=generateUUIDFilename)
+    originalFileName = models.CharField('Original filename', max_length=300, editable=False)
+
+    # *****Meta and clean*****
+    class Meta:
+        verbose_name = "Mentor Event File"
+
+
+    # *****Permissions*****
+    # @classmethod
+    # def coordinatorPermissions(cls, level):
+    #     return eventCoordinatorViewPermissions(level)
+
+    # *****Save & Delete Methods*****
+
+    # *****Methods*****
+
+    # *****Get Methods*****
+
+    # def __str__(self):
+    #     return str(self.year)
+
+    def filesize(self):
+        return formatFilesize(self.fileUpload.size)
+    filesize.short_description = 'Size'
+
+    # *****CSV export methods*****
+
+    # *****Email methods*****
 
