a6bd1f13df334b424ac402baffbb6d0116ec8e8b
Peter Drew <peter@pdrew.com>
2020-10-18 23:23:57 +1100

    add association app

--- rcjaRegistration/rcjaRegistration/settings_test.py
+++ rcjaRegistration/rcjaRegistration/settings_test.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'invoices.apps.InvoicesConfig',
     'schools.apps.SchoolsConfig',
     'workshops.apps.WorkshopsConfig',
+    'association.apps.AssociationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
