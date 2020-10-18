11fd6b51e79ec2d9e65e72c57f4b8c12f5a6faac
Peter Drew <peter@pdrew.com>
2020-10-18 21:14:23 +1100

    delete public website app folder

--- rcjaRegistration/publicwebsite/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from common.admin import ExportCSVMixin
-from coordination.adminPermissions import AdminPermissions
-
-# Register your models here.
--- rcjaRegistration/publicwebsite/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PublicwebsiteConfig(AppConfig):
-    name = 'publicwebsite'
-    verbose_name = 'Public website'
