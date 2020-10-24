449a79f57fbf461b8f7fd017c263aa5526b5c504
Peter Drew <peter@pdrew.com>
2020-10-24 18:46:05 +1100

    adjust team and workshop attendee string methods

--- rcjaRegistration/teams/models.py
+++ rcjaRegistration/teams/models.py
@@ -103,7 +103,7 @@ class Team(BaseEventAttendance):
     # *****Get Methods*****
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.event.name})"
 
     # *****CSV export methods*****
 
