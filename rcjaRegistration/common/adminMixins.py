bb5189d2514e5217f00a68a15407c0689ef4de53
Peter Drew <peter@pdrew.com>
2020-10-24 15:26:15 +1100

    improve response_add comments

--- rcjaRegistration/common/adminMixins.py
+++ rcjaRegistration/common/adminMixins.py
@@ -117,14 +117,14 @@ class DifferentAddFieldsMixin:
     def response_add(self, request, obj, post_url_continue=None):
         """
         Determine the HttpResponse for the add_view stage. It mostly defers to
-        its superclass implementation but is customized because the User model
-        has a slightly different workflow.
+        its superclass implementation but is customized to show the change page
+        even if save clicked. Is based on the User response_add function.
         """
-        # We should allow further modification of the user just added i.e. the
+        # We should allow further modification of the object just added i.e. the
         # 'Save' button should behave like the 'Save and continue editing'
         # button except in two scenarios:
         # * The user has pressed the 'Save and add another' button
-        # * We are adding a user in a popup
+        # * We are adding an object in a popup
         if '_addanother' not in request.POST and IS_POPUP_VAR not in request.POST:
             request.POST = request.POST.copy()
             request.POST['_continue'] = 1
