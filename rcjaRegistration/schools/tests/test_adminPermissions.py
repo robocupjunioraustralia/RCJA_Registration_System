3b4f7f2b42d6e0a915dc74b7fc30b6012b036c92
Peter Drew <peter@pdrew.com>
2021-01-04 23:47:42 +1100

    add new school administrator admin permissions tests

--- rcjaRegistration/schools/tests/test_adminPermissions.py
+++ rcjaRegistration/schools/tests/test_adminPermissions.py
@@ -83,4 +83,67 @@ class Test_School_FullCoordinator(School_Coordinators_Base, Base_Test_FullCoordi
 class Test_School_ViewCoordinator(School_Coordinators_Base, Base_Test_ViewCoordinator, TestCase):
     pass
 
+# School Administrator
 
+class SchoolAdministrator_Base:
+    modelURLName = 'schools_schooladministrator'
+    state1Obj = 'schooladmin_mentor1'
+    state2Obj = 'schooladmin_mentor4'
+    validPayload = {
+            'user': 0,
+            'school': 0,
+        }
+
+    def updatePayload(self):
+        self.validPayload['user'] = self.user_state2_school3_mentor4.id # Can edit users from any state, is only the school field that is filtered
+        self.validPayload['school'] = self.school1_state1.id
+
+class Test_SchoolAdministrator_NotStaff(SchoolAdministrator_Base, Base_Test_NotStaff, TestCase):
+    pass
+
+class Test_SchoolAdministrator_SuperUser(SchoolAdministrator_Base, Base_Test_SuperUser, TestCase):
+    expectedListItems = 4
+    expectedStrings = [
+        'School 1',
+        'School 2',
+        'School 3',
+    ]
+    expectedMissingStrings = []
+
+    def testPostAddBlankSchool(self):
+        payload = self.validPayload.copy()
+        del payload['school']
+        response = self.client.post(reverse(f'admin:{self.modelURLName}_add'), data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Please correct the errors below.') # Multiple errors because of checkRequiredFieldsNotNone validation
+        self.assertContains(response, 'This field is required.')
+
+class SchoolAdministrator_Coordinators_Base(SchoolAdministrator_Base):
+    expectedListItems = 3
+    expectedStrings = [
+        'School 1',
+        'School 2',
+    ]
+    expectedMissingStrings = [
+        'School 3',
+    ]
+
+class Test_SchoolAdministrator_FullCoordinator(SchoolAdministrator_Coordinators_Base, Base_Test_FullCoordinator, TestCase):
+    def testPostAddBlankSchool(self):
+        payload = self.validPayload.copy()
+        del payload['school']
+        response = self.client.post(reverse(f'admin:{self.modelURLName}_add'), data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Please correct the errors below.') # Multiple errors because of checkRequiredFieldsNotNone validation
+        self.assertContains(response, 'This field is required.')
+
+    def testPostAddSchoolWrongState(self):
+        payload = self.validPayload.copy()
+        payload['school'] = self.school4_state2.id
+        response = self.client.post(reverse(f'admin:{self.modelURLName}_add'), data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Please correct the errors below.') # Multiple errors because of checkRequiredFieldsNotNone validation
+        self.assertContains(response, 'Select a valid choice. That choice is not one of the available choices.')
+
+class Test_SchoolAdministrator_ViewCoordinator(SchoolAdministrator_Coordinators_Base, Base_Test_ViewCoordinator, TestCase):
+    pass
