14e6fbe8cafedfd27116bbe398b839904224d859
Peter Drew <peter@pdrew.com>
2021-01-06 13:35:40 +1100

    update state actions

--- rcjaRegistration/publicapi/views.py
+++ rcjaRegistration/publicapi/views.py
@@ -35,22 +35,22 @@ class StateViewSet(viewsets.ReadOnlyModelViewSet, NestedSerializerActionMinxin):
 
     permission_classes = (ReadOnly,)
 
-    def eventsBaseQueryset(self, pk):
-        state = get_object_or_404(State, pk=pk, typeWebsite=True)
+    def eventsBaseQueryset(self, abbreviation):
+        state = get_object_or_404(State, abbreviation=abbreviation, typeWebsite=True)
         if state.typeGlobal:
             return Event.objects.filter(globalEvent=True, status='published')
         else:
             return Event.objects.filter(globalEvent=False, state=state, status='published')
 
     @action(detail=True)
-    def upcomingEvents(self, request, pk=None):
-        queryset = self.eventsBaseQueryset(pk).filter(startDate__gte=datetime.datetime.today()).order_by('startDate')
+    def upcomingEvents(self, request, abbreviation=None):
+        queryset = self.eventsBaseQueryset(abbreviation).filter(startDate__gte=datetime.datetime.today()).order_by('startDate')
         return self.nestedSerializer(queryset, EventSerializer)
 
     @action(detail=True)
-    def pastEvents(self, request, pk=None):
+    def pastEvents(self, request, abbreviation=None):
         # May want to limit the past events that are available
-        queryset = self.eventsBaseQueryset(pk).filter(startDate__lt=datetime.datetime.today()).order_by('-startDate')
+        queryset = self.eventsBaseQueryset(abbreviation).filter(startDate__lt=datetime.datetime.today()).order_by('-startDate')
         return self.nestedSerializer(queryset, EventSerializer)
 
     # @action(detail=True)
