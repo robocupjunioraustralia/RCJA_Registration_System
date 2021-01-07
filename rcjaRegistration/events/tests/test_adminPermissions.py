27dcb3d219c09648cccc4c531a84198b362b265a
Peter Drew <peter@pdrew.com>
2021-01-07 12:42:01 +1100

    add new venue tests and make validation more roboust

--- rcjaRegistration/events/tests/test_adminPermissions.py
+++ rcjaRegistration/events/tests/test_adminPermissions.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 
 from events.models import Event, Division, Year, AvailableDivision, Venue
 
-# State
+# Division
 
 class Division_Base:
     modelURLName = 'events_division'
@@ -73,5 +73,69 @@ class Test_Division_FullCoordinator(Division_Coordinators_Base, Base_Test_FullCo
 class Test_Division_ViewCoordinator(Division_Coordinators_Base, Base_Test_ViewCoordinator, TestCase):
     pass
 
+# Venue
 
+class Venue_Base:
+    modelURLName = 'events_venue'
+    state1Obj = 'venue1_state1'
+    state2Obj = 'venue3_state2'
+    validPayload = {
+            'name': 'New Venue',
+            'state': 0,
+        }
 
+    def additionalSetup(self):
+        createEvents(self)
+
+    def updatePayload(self):
+        self.validPayload['state'] = self.state1.id
+
+class Test_Venue_NotStaff(Venue_Base, Base_Test_NotStaff, TestCase):
+    pass
+
+class Test_Venue_SuperUser(Venue_Base, Base_Test_SuperUser, TestCase):
+    expectedListItems = 3
+    expectedStrings = [
+        'Venue 1',
+        'Venue 2',
+        'Venue 3',
+    ]
+    expectedMissingStrings = []
+
+    def testPostAddBlankState(self):
+        payload = self.validPayload.copy()
+        del payload['state']
+        response = self.client.post(reverse(f'admin:{self.modelURLName}_add'), data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Please correct the errors below.') # Multiple errors because of checkRequiredFieldsNotNone validation
+        self.assertContains(response, 'This field is required.')
+
+class Venue_Coordinators_Base(Venue_Base):
+    expectedListItems = 2
+    expectedStrings = [
+        'Venue 1',
+        'Venue 2',
+    ]
+    expectedMissingStrings = [
+        'Venue 3',
+    ]
+
+class Test_Venue_FullCoordinator(Venue_Coordinators_Base, Base_Test_FullCoordinator, TestCase):
+    def testPostAddBlankState(self):
+        payload = self.validPayload.copy()
+        del payload['state']
+        response = self.client.post(reverse(f'admin:{self.modelURLName}_add'), data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Please correct the errors below.') # Multiple errors because of checkRequiredFieldsNotNone validation
+        self.assertContains(response, 'This field is required.')
+
+    def testPostAddWrongState(self):
+        payload = self.validPayload.copy()
+        payload['state'] = self.state2.id
+        response = self.client.post(reverse(f'admin:{self.modelURLName}_add'), data=payload)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Please correct the errors below.') # Multiple errors because of checkRequiredFieldsNotNone validation
+        self.assertContains(response, 'Select a valid choice. That choice is not one of the available choices.')
+
+class Test_Venue_ViewCoordinator(Venue_Coordinators_Base, Base_Test_ViewCoordinator, TestCase):
+    pass
