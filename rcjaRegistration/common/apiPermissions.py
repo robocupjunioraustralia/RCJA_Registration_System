8f06e909e828514431dcc16c3455423fba21bb6b
Peter Drew <peter@pdrew.com>
2021-01-16 12:20:28 +1100

    add is active check to api permissions

--- rcjaRegistration/common/apiPermissions.py
+++ rcjaRegistration/common/apiPermissions.py
@@ -4,6 +4,7 @@ class IsSuperUser(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.user and
+            request.user.is_active and
             request.user.is_superuser
         )
 
@@ -11,6 +12,7 @@ class AuthenticatedReadOnly(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.user and
+            request.user.is_active and
             request.user.is_authenticated and
             request.method in SAFE_METHODS
         )
