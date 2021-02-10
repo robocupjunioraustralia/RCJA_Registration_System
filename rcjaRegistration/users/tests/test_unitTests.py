5cce2c2550f947896b177b893c8409462e4e6461
Peter Drew <peter@pdrew.com>
2021-02-10 15:37:55 +1100

    cleanup user unit tests imports

--- rcjaRegistration/users/tests/test_unitTests.py
+++ rcjaRegistration/users/tests/test_unitTests.py
@@ -1,8 +1,4 @@
-from django.contrib.auth import get_user_model
-from django.test import SimpleTestCase, TestCase, modify_settings
-from django.urls import reverse
-from django.test import Client
-from django.http import HttpRequest
+from django.test import TestCase
 from django.core.exceptions import ValidationError
 from unittest.mock import patch, Mock
 
