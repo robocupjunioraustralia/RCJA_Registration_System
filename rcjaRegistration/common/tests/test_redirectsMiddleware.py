308d069d2e7e72a375e1ebf5e246e19f134e8a24
Peter Drew <peter@pdrew.com>
2023-09-06 15:43:24 +1000

    add admin changelog redirect tests

--- rcjaRegistration/common/tests/test_redirectsMiddleware.py
+++ rcjaRegistration/common/tests/test_redirectsMiddleware.py
@@ -47,6 +47,13 @@ class Test_redirectsMiddleware_notStaff(Base_Tests_redirectsMiddleware, TestCase
         self.login = self.client.login(request=HttpRequest(), username=self.email_user_state1_school1_mentor1, password=self.password)
         self.user = self.user_state1_school1_mentor1
 
+    def testNoRedirectAdminChangelog(self):
+        self.user.adminChangelogVersionShown = 0
+        self.user.save()
+
+        response = self.client.get(reverse('events:dashboard'))
+        self.assertEqual(response.status_code, 200)
+
     def testSchoolDetailsUpdateRedirect(self):
         self.user.currentlySelectedSchool.forceSchoolDetailsUpdate = True
         self.user.currentlySelectedSchool.save()
@@ -64,3 +71,25 @@ class Test_redirectsMiddleware_notStaff(Base_Tests_redirectsMiddleware, TestCase
 
         response = self.client.get(reverse('events:dashboard'))
         self.assertEqual(response.status_code, 200)
+
+class Test_redirectsMiddleware_superuser(Base_Tests_redirectsMiddleware, TestCase):
+    def setUp(self):
+        self.login = self.client.login(request=HttpRequest(), username=self.email_user_state1_super1, password=self.password)
+        self.user = self.user_state1_super1
+
+    def testRedirectAdminChangelog(self):
+        self.user.adminChangelogVersionShown = 0
+        self.user.save()
+
+        response = self.client.get(reverse('events:dashboard'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('users:adminChangelog'))
+
+    def testUpdate_adminChangelogVersionShown(self):
+        self.user.adminChangelogVersionShown = 0
+        self.user.save()
+        self.assertNotEqual(self.user.adminChangelogVersionShown, self.user.ADMIN_CHANGELOG_CURRENT_VERSION)
+
+        response = self.client.get(reverse('events:dashboard'))
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.adminChangelogVersionShown, self.user.ADMIN_CHANGELOG_CURRENT_VERSION)
