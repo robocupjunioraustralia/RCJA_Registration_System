d5c5bbc761438072ae74685ad30c6d5410a76c41
Peter Drew <peter@pdrew.com>
2023-09-06 14:25:35 +1000

    Add more tests

--- rcjaRegistration/common/tests/test_redirectsMiddleware.py
+++ rcjaRegistration/common/tests/test_redirectsMiddleware.py
@@ -15,15 +15,50 @@ class Test_redirectsMiddleware_notStaff(TestCase):
 
     def setUp(self):
         self.login = self.client.login(request=HttpRequest(), username=self.email_user_state1_school1_mentor1, password=self.password)
+        self.user = self.user_state1_school1_mentor1
 
     def testNoRedirect(self):
         response = self.client.get(reverse('events:dashboard'))
         self.assertEqual(response.status_code, 200)
 
     def testPasswordChangeRedirect(self):
-        self.user_state1_school1_mentor1.forcePasswordChange = True
-        self.user_state1_school1_mentor1.save()
+        self.user.forcePasswordChange = True
+        self.user.forceDetailsUpdate = True
+        self.user.save()
+
+        self.user.currentlySelectedSchool.forceSchoolDetailsUpdate = True
+        self.user.currentlySelectedSchool.save()
 
         response = self.client.get(reverse('events:dashboard'))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('password_change'))
+
+    def testNoRedirectTermsAndConditions(self):
+        self.user.forcePasswordChange = True
+        self.user.forceDetailsUpdate = True
+        self.user.save()
+
+        self.user.currentlySelectedSchool.forceSchoolDetailsUpdate = True
+        self.user.currentlySelectedSchool.save()
+
+        response = self.client.get(reverse('users:termsAndConditions'))
+        self.assertEqual(response.status_code, 200)
+
+    def testUserDetailsUpdateRedirect(self):
+        self.user.forceDetailsUpdate = True
+        self.user.save()
+
+        self.user.currentlySelectedSchool.forceSchoolDetailsUpdate = True
+        self.user.currentlySelectedSchool.save()
+
+        response = self.client.get(reverse('events:dashboard'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('users:details'))
+
+    def testSchoolDetailsUpdateRedirect(self):
+        self.user.currentlySelectedSchool.forceSchoolDetailsUpdate = True
+        self.user.currentlySelectedSchool.save()
+
+        response = self.client.get(reverse('events:dashboard'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse('schools:details'))
