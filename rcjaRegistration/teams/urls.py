5078d65328f446e51fa928105cc9574781908113
Peter Drew <peter@pdrew.com>
2023-09-29 15:37:07 +1000

    Add team copy logic

--- rcjaRegistration/teams/urls.py
+++ rcjaRegistration/teams/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('teams/<int:teamID>/edit', views.CreateEditTeam.as_view(), name='edit'),
     path('teams/<int:teamID>', views.details, name='details'),
     path('teams/copyExisting/<int:eventID>', views.copyTeamsList, name='copyTeamsList'),
+    path('teams/copyExisting/<int:eventID>/create/<int:teamID>', views.copyTeam, name='copyTeam'),
 ]
