efc90f4870e5ad96810ba1fb9f61506e52f6aa67
Peter Drew <peter@pdrew.com>
2023-09-30 15:57:07 +1000

    Add association member model and admin

--- rcjaRegistration/association/admin.py
+++ rcjaRegistration/association/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from common.adminMixins import ExportCSVMixin, DifferentAddFieldsMixin, FKActionsRemove
+from coordination.permissions import AdminPermissions, InlineAdminPermissions
+
+from .models import AssociationMember
+from users.admin import UserAdmin
+from users.models import User
 
 # Register your models here.
+
+@admin.register(AssociationMember)
+class SchoolAdmin(FKActionsRemove, AdminPermissions, admin.ModelAdmin, ExportCSVMixin):
+    list_display = [
+        '__str__',
+    ]
+    list_filter = [
+        ('user__homeState', admin.RelatedOnlyFieldListFilter),
+    ]
+    search_fields = [
+        'user__first_name',
+        'user__last_name',
+        'user__email',
+        'user__homeState__name',
+    ]
+    autocomplete_fields = [
+        'user'
+    ]
+    actions = [
+        'export_as_csv',
+    ]
+    exportFields = [
+        'pk',
+        'user',
+        'birthday',
+        'address',
+        'membershipStartDate',
+        'membershipEndDate',
+    ]
+
+    # State based filtering
+
+    fkFilterFields = {
+        'user': {
+            'fieldAdmin': UserAdmin,
+            'fieldModel': User,
+        },
+    }
+
+    statePermissionsFilterLookup = 'user__homeState__coordinator'
--- rcjaRegistration/association/models.py
+++ rcjaRegistration/association/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from common.models import SaveDeleteMixin, checkRequiredFieldsNotNone
+from django.conf import settings
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+# **********MODELS**********
+
+class AssociationMember(SaveDeleteMixin, models.Model):
+    # Foreign keys
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name='User', on_delete=models.PROTECT)
+    # Creation and update time
+    creationDateTime = models.DateTimeField('Creation date',auto_now_add=True)
+    updatedDateTime = models.DateTimeField('Last modified date',auto_now=True)
+    # Fields
+    birthday = models.DateField('Birthday')
+    address = models.TextField('Address', blank=True)
+    membershipStartDate = models.DateField('Membership start date', null=True, blank=True)
+    membershipEndDate = models.DateField('Membership end date', null=True, blank=True)
+
+    # *****Meta and clean*****
+    class Meta:
+        verbose_name = 'Association Member'
+        ordering = ['user']
+
+    def clean(self):
+        errors = {}
+
+        if self.membershipEndDate:
+            if not self.membershipStartDate:
+                errors['membershipStartDate'] = 'Membership start date must not be blank if membership end date set'
+            
+            elif self.membershipStartDate >= self.membershipEndDate:
+                errors['membershipStartDate'] = 'Membership start date must be before membership end date'
+
+        # Raise any errors
+        if errors:
+            raise ValidationError(errors)
+
+    # *****Permissions*****
+    @classmethod
+    def stateCoordinatorPermissions(cls, level):
+        if level in ['full', 'associationmanager']:
+            return [
+                'add',
+                'view',
+                'change',
+                'delete'
+            ]
+        elif level in ['viewall']:
+            return [
+                'view',
+            ]
+
+        return []
+
+    # Used in state coordinator permission checking
+    def getState(self):
+        return self.user.homeState
+
+    # *****Save & Delete Methods*****
+
+    # *****Methods*****
+
+    # *****Get Methods*****
+
+    def __str__(self):
+        return str(self.user)
+
+    # *****CSV export methods*****
+
+    # *****Email methods*****
