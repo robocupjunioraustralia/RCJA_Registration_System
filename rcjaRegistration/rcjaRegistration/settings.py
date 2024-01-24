b46a36e483f20156cd9a77bf3488b875afc6e8ed
evanbailey <evan@baileyfinance.com.au>
2024-01-24 13:56:50 +1100

    Add additional import of DjangoIntegration from sentry_sdk to squash flake8 error

--- rcjaRegistration/rcjaRegistration/settings.py
+++ rcjaRegistration/rcjaRegistration/settings.py
@@ -15,6 +15,7 @@ import environ
 import sys
 from opencensus.trace import config_integration
 import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
