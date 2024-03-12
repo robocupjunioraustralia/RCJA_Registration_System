4ca38af98a8fd9c12728bf75300ba8e9628eeba7
Zac McWilliam <zac@mcwilliam.net.au>
2024-03-12 22:13:36 +1100

    Add default event fee value

--- rcjaRegistration/events/models.py
+++ rcjaRegistration/events/models.py
@@ -267,7 +267,7 @@ class Event(SaveDeleteMixin, models.Model):
 
     # Billing details
     entryFeeIncludesGST = models.BooleanField('Includes GST', default=True, help_text='Whether the prices specified on this page are GST inclusive or exclusive.')
-    event_defaultEntryFee = models.PositiveIntegerField('Default entry fee')
+    event_defaultEntryFee = models.PositiveIntegerField('Default entry fee', default=0)
     paymentDueDate = models.DateField('Payment due date', null=True, blank=True)
 
     # Competition billing settings
