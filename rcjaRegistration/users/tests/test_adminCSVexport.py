5a863fb706ea4aa470f93f389dcb8d1caebc1340
Peter Drew <peter@pdrew.com>
2024-11-03 14:56:18 +1100

    Add user question and response CSV export tests

--- rcjaRegistration/users/tests/test_adminCSVexport.py
+++ rcjaRegistration/users/tests/test_adminCSVexport.py
@@ -1,4 +1,4 @@
-from common.baseTests import createStates, createUsers
+from common.baseTests import createStates, createUsers, createQuestionsAndResponses
 from django.http import HttpRequest
 from django.urls import reverse
 from django.test import TestCase
@@ -8,6 +8,7 @@ class TestAdminCSVExport_User(TestCase):
     def setUpTestData(cls):
         createStates(cls)
         createUsers(cls)
+        createQuestionsAndResponses(cls)
 
     def setUp(self):
         self.client.login(request=HttpRequest(), username=self.email_user_state1_super1, password=self.password)
@@ -19,7 +20,11 @@ class TestAdminCSVExport_User(TestCase):
     def test_export_as_csv_correctHeaders(self):
         response = self.client.post(reverse('admin:users_user_changelist'), {'action': 'export_as_csv', '_selected_action': [1]})
         self.assertContains(response, 'Home state')
+        self.assertContains(response, 'Consent')
+        self.assertContains(response, 'Marketing')
 
     def test_export_as_csv_correctValues(self):
         response = self.client.post(reverse('admin:users_user_changelist'), {'action': 'export_as_csv', '_selected_action': [1]})
         self.assertContains(response, 'State 1')
+        self.assertContains(response, 'True')
+        self.assertContains(response, 'False')
