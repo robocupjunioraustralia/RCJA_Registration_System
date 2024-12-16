0ead55c9ef1c2a188049550be499199252ef89a5
Harr0101 <Harry.N123@outlook.com>
2024-12-16 13:28:34 +1000

    Add documentation

--- rcjaRegistration/common/userInterface.py
+++ rcjaRegistration/common/userInterface.py
@@ -2,6 +2,7 @@ from os import environ
 from django.shortcuts import render as django_render
 
 def render(request, template_name, context=None, *args, **kwargs):
+    """ Add development tag to header if development environment is active """
     if type(context) == dict:
         context["development"] = environ["DEV_SETTINGS"]
     else:
