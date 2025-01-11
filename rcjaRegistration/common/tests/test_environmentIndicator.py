1261f40f3afbb8d0344938a8ecd9a71b4acdcff1
Peter Drew <peter@pdrew.com>
2025-01-11 20:15:09 +0000

    Add environment indicator tests

---
+++ rcjaRegistration/common/tests/test_environmentIndicator.py
@@ -0,0 +1,23 @@
+from common.baseTests import createStates, createUsers
+from django.test import TestCase, override_settings
+from django.urls import reverse
+
+from django.http import HttpRequest
+
+class Test_EnvironmentIndicator(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        createStates(cls)
+        createUsers(cls)
+
+    def setUp(self):
+        self.login = self.client.login(request=HttpRequest(), username=self.email_user_state1_school1_mentor1, password=self.password)
+
+    def test_testingDisplayed(self):
+        response = self.client.get(reverse('events:dashboard'))
+        self.assertContains(response, 'TESTING')
+    
+    @override_settings(ENVIRONMENT='PRODUCTION')
+    def test_noIndiactorInProduction(self):
+        response = self.client.get(reverse('events:dashboard'))
+        self.assertNotContains(response, 'TESTING')
