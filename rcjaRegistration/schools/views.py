3864e66800debbdff2412450f75fe901cca8bc2a
Peter Drew <peter@pdrew.com>
2025-01-11 19:59:22 +0000

    Revert views.py changes

--- rcjaRegistration/schools/views.py
+++ rcjaRegistration/schools/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.template import loader
