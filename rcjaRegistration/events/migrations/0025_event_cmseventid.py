e0cc1c44d0df3c3e0d324e3c73775e0cb66d2fbf
Zac McWilliam <zac@mcwilliam.net.au>
2025-01-12 20:50:51 +1100

    RC-1106 - Dedupe event ID migration

--- rcjaRegistration/events/migrations/0025_event_cmseventid.py
+++ rcjaRegistration/events/migrations/0025_event_cmseventid.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('events', '0023_division_active'),
+        ('events', '0024_alter_availabledivision_division'),
     ]
 
     operations = [
