78283580a1c8ba7e21884a64a0a3eb79031ae6ce
Peter Drew <peter@pdrew.com>
2025-02-02 16:23:41 +1100

    Fix tests

--- rcjaRegistration/schools/tests/test_mergeSchools.py
+++ rcjaRegistration/schools/tests/test_mergeSchools.py
@@ -19,7 +19,7 @@ class Test_MergeSchools_notstaff(Base_Test_MergeSchools, TestCase):
         self.client.login(request=HttpRequest(), username=self.email_user_notstaff, password=self.password)
 
     def test_permission_denied(self):
-        response = self.client.get(reverse('schools:mergeSchools', args=[self.school1_state1.id, self.school2_state1.id]))
+        response = self.client.get(reverse('schools:adminMergeSchools', args=[self.school1_state1.id, self.school2_state1.id]))
         self.assertEqual(response.status_code, 403)
 
 class Test_MergeSchools_staff_otherstate(Base_Test_MergeSchools, TestCase):
@@ -27,7 +27,7 @@ class Test_MergeSchools_staff_otherstate(Base_Test_MergeSchools, TestCase):
         self.client.login(request=HttpRequest(), username=self.email_user_state2_fullcoordinator, password=self.password)
 
     def test_permission_denied(self):
-        response = self.client.get(reverse('schools:mergeSchools', args=[self.school1_state1.id, self.school2_state1.id]))
+        response = self.client.get(reverse('schools:adminMergeSchools', args=[self.school1_state1.id, self.school2_state1.id]))
         self.assertEqual(response.status_code, 403)
 
 class Test_MergeSchools_superuser(Base_Test_MergeSchools, TestCase):    
@@ -35,5 +35,9 @@ class Test_MergeSchools_superuser(Base_Test_MergeSchools, TestCase):
         self.client.login(request=HttpRequest(), username=self.email_user_state1_super1, password=self.password)
 
     def test_page_loads(self):
-        response = self.client.get(reverse('schools:mergeSchools', args=[self.school1_state1.id, self.school2_state1.id]))
+        response = self.client.get(reverse('schools:adminMergeSchools', args=[self.school1_state1.id, self.school2_state1.id]))
         self.assertEqual(response.status_code, 200)
+
+    def test_uses_correct_template(self):
+        response = self.client.get(reverse('schools:adminMergeSchools', args=[self.school1_state1.id, self.school2_state1.id]))
+        self.assertTemplateUsed(response, 'schools/adminMergeSchools.html')
