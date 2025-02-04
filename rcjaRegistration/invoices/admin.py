d9c0f4f1c93d538a53d404f1c6104920a559e882
Peter Drew <peter@pdrew.com>
2025-02-04 23:28:50 +1100

    Restrict invoice deletion to superusers and fix tests

--- rcjaRegistration/invoices/admin.py
+++ rcjaRegistration/invoices/admin.py
@@ -215,6 +215,9 @@ class InvoiceAdmin(AdminPermissions, admin.ModelAdmin, ExportCSVMixin):
     # Prevent add because always created by signal
     # Reconsider in conjuction with signals
     def has_delete_permission(self, request, obj=None):
+        if not request.user.is_superuser:
+            return False
+        
         if obj:
             if obj.invoicepayment_set.exists() or obj.invoiceAmountInclGST_unrounded() > 0.05:
                 return False
